87b185f9d24a1d91bd85a976a57726aa76ade280
justice sommer <boxmaker@gmail.com>
2019-03-19 15:58:52 -0700

    no message

--- test/create.js
+++ test/create.js
@@ -14,6 +14,7 @@ var Page = require('../lib/tasks');
 var username = sourceFile_credentials.credentials_a['wonka_tester']['username']
 var password = sourceFile_credentials.credentials_a['wonka_tester']['password']
 chai.use(chaiAsPromised);
+var assert = chai.assert;
 
 
 for (var i = adminBrowserss.length - 1; i >= 0; i--) {
@@ -53,8 +54,7 @@ function tests(browser){
     });
     afterEach(function(){
       // if (this.currentTest.state == 'failed') {
-      //   addContext(this, 'screenshots/'+testImageName+'.png');
-      //   page.screenshot(testImageName)
+      //   addContext('This test failed');
       // }
       addContext(this, 'screenshots/'+this.currentTest.title.replace(/ /g,"_")+'.png');
       page.screenshot(this.currentTest.title.replace(/ /g,"_"))
@@ -65,27 +65,13 @@ function tests(browser){
       page.quit();
     });
 
-
-    // it('Create a lead', function(done){
-    //   this.retries(trys)
-    //   var firstName = page.randomString(10,"alpha");
-    //   var lastName = page.randomString(10,"alpha");
-    //   // var build = page.getBuild()
-
-    //   page.clickCreateOption(1)
-    //   .then(() => page.fillNewLead(firstName,lastName))
-    //   .then(() => page.clickCreateButtonNewLeadModal())
-    //   .then(() => sleep(500))
-
-    //   var verificationText = page.getInnerHTML('/html/body/div[1]/div/div/parent-crm-modal/div/div[1]/div/h4/div/span[1]','xpath');
-
-    //   verificationText.txt.should.eventually.equal(firstName+" "+lastName, "The Lead was not created properly").notify(done);
-    // });
     function createLeadTest(leadSourceType){
       it('Create a lead with Lead Source set to '+leadSourceType+' and search for it', function(done){
         this.retries(trys)
         if(leadSourceType == "Seminar/Workshop" ){
           var courseID = page.getACourseID('Seminar')
+        }else if (leadSourceType == "Parent Event"){
+         var courseID = page.getACourseID('Parent Event')
         }else if (leadSourceType == "Mock Exam"){
          var courseID = page.getACourseID('Mock Exam')
         }else{
@@ -96,6 +82,7 @@ function tests(browser){
 
         page.clickCreateOption(1)
         .then(() => page.fillNewLead(firstName,lastName,leadSourceType,courseID))
+        .then(() => sleep(500))
         .then(() => page.clickCreateButtonNewLeadModal())
         .then(() => sleep(500))
         .then(() => page.clickXtoCloseCRM())
@@ -129,37 +116,131 @@ function tests(browser){
       createLeadTest(leadSourceTypes[i])
     }
 
-    // it('Create a school and search for it', function(done){
-    //   this.retries(trys)
-    //   var name = page.randomString(10,"alpha");
+    it('Create a lead with VIP', function(done){
+      this.retries(trys)
+      // var firstName = page.randomString(10,"alpha");
+      // var lastName = page.randomString(10,"alpha");
+      page.clickCreateOption(1)
+      // .then(() => page.fillNewLead(firstName,lastName))
+      .then(() => page.fillNewLead())
+      .then(() => page.toggleVIPNewLeadModal())
+      .then(() => page.clickCreateButtonNewLeadModal())
+      .then(() => sleep(5000))
+      .then(() => page.getDiamonds())
+      .then((diamonds) => assert.lengthOf(diamonds, 1, "The diamond did not appear properly"))
+      .then(() => done())
+    });
 
-    //   page.clickCreateOption(2)
-    //   .then(() => page.fillNewSchool(name))
-    //   .then(() => page.clickCreateButtonNewSchoolModal())
-    //   .then(() => sleep(500))
-    //   .then(() => searchForLeadSource(name))
-    //   .then(() => sleep(5000))
-    //   .then(() => {
-    //     var verificationText = page.getInnerHTML('/html/body/ui-view/app/div/div/div/div/ui-view/crm-lead-sources/div/div[2]/crm-lead-sources-results/div/div/div/table/tbody/tr/td[2]/a','xpath')
-    //     verificationText.txt.should.eventually.include(name, "The newly created school did not appear as the search result").notify(done);
-    //   })
-    // });
+    it('Create a lead with a taken email', function(done){
+      this.retries(trys)
+      page.clickCreateOption(1)
+      .then(() => page.fillNewLead( false, false,username, "", ""))
+      .then(() => {
+        var verificationText = page.getErrorTextNewLeadModal()
+        verificationText.txt.should.eventually.include("This email is already taken!", "The error for using a taken email did not appear properly").notify(done);
+      })
+    });
 
-    // it('Create a lead source and search for it', function(done){
-    //   this.retries(trys)
-    //   var name = page.randomString(10,"alpha");
+    it('Create a lead with a student', function(done){
+      this.retries(trys)
+      var firstName = page.randomString(10,"alpha");
+      var lastName = page.randomString(10,"alpha");
+      var studentFirstName = page.randomString(10,"alpha");
+      var studentLastName = page.randomString(10,"alpha");
+      page.clickCreateOption(1)
+      .then(() => page.fillNewLead(firstName,lastName))
+      .then(() => page.clickAddStudentNewLeadModal())
+      .then(() => page.fillStudentNewLeadModal(1,studentFirstName,studentLastName))
+      .then(() => page.clickCreateButtonNewLeadModal())
+      .then(() => sleep(5000))
+      .then(() => page.getDiamonds())
+      .then(() => {
+        var studentVerificationText = page.getInnerHTML('/html/body/div[1]/div/div/parent-crm-modal/div/div[2]/div[1]/div[1]/div[1]/div/div/a','xpath')
 
-    //   page.clickCreateOption(3)
-    //   .then(() => page.fillNewLeadSource(name))
-    //   .then(() => page.clickCreateButtonNewLeadSourceModal())
-    //   .then(() => sleep(500))
-    //   .then(() => searchForLeadSource(name))
-    //   .then(() => sleep(5000))
-    //   .then(() => {
-    //     var verificationText = page.getInnerHTML('/html/body/ui-view/app/div/div/div/div/ui-view/crm-lead-sources/div/div[2]/crm-lead-sources-results/div/div/div/table/tbody/tr/td[2]/a','xpath')
-    //     verificationText.txt.should.eventually.include(name, "The newly created Lead source did not appear as the search result").notify(done);
-    //   })
-    // });
+        studentVerificationText.txt.should.eventually.include(studentFirstName+" "+studentLastName, "The students name ("+studentFirstName+" "+studentLastName+") did not appear properly")
+      })
+      .then(() => {
+        var verificationText = page.getInnerHTML('/html/body/div[1]/div/div/parent-crm-modal/div/div[1]/div/h4/div/span[1]','xpath')
+        verificationText.txt.should.eventually.include(firstName+" "+lastName, "The parents name ("+firstName+" "+lastName+") did not appear properly").notify(done);
+      })
+    });
+
+    it('Create a lead with two students', function(done){
+      this.retries(trys)
+      var firstName = page.randomString(10,"alpha");
+      var lastName = page.randomString(10,"alpha");
+      var studentFirstName = page.randomString(10,"alpha");
+      var studentLastName = page.randomString(10,"alpha");
+      var student2FirstName = page.randomString(10,"alpha");
+      var student2LastName = page.randomString(10,"alpha");
+      page.clickCreateOption(1)
+      .then(() => page.fillNewLead(firstName,lastName))
+      .then(() => page.clickAddStudentNewLeadModal())
+      .then(() => page.fillStudentNewLeadModal(1,studentFirstName,studentLastName))
+      .then(() => page.clickAddStudentNewLeadModal())
+      .then(() => page.fillStudentNewLeadModal(2,student2FirstName,student2LastName))
+      .then(() => page.clickCreateButtonNewLeadModal())
+      .then(() => sleep(5000))
+      .then(() => page.getDiamonds())
+      .then(() => {
+        var studentVerificationText = page.getInnerHTML('/html/body/div[1]/div/div/parent-crm-modal/div/div[2]/div[1]/div[1]/div[1]/div/div/a','xpath')
+
+        studentVerificationText.txt.should.eventually.include(studentFirstName+" "+studentLastName, "The students name ("+studentFirstName+" "+studentLastName+") did not appear properly")
+      })
+      .then(() => {
+        var studentVerificationText = page.getInnerHTML('/html/body/div[1]/div/div/parent-crm-modal/div/div[2]/div[1]/div[1]/div[1]/div/div[2]/a','xpath')
+
+        studentVerificationText.txt.should.eventually.include(student2FirstName+" "+student2LastName, "The students name ("+student2FirstName+" "+student2LastName+") did not appear properly")
+      })
+      .then(() => {
+        var verificationText = page.getInnerHTML('/html/body/div[1]/div/div/parent-crm-modal/div/div[1]/div/h4/div/span[1]','xpath')
+        verificationText.txt.should.eventually.include(firstName+" "+lastName, "The parents name ("+firstName+" "+lastName+") did not appear properly").notify(done);
+      })
+    });
+
+
+    function createSchoolTest(categorie,type){
+      it('Create a '+type+' '+categorie+' and search for it', function(done){
+        this.retries(trys)
+        var name = page.randomString(10,"alpha");
+
+        page.clickCreateOption(2)
+        .then(() => page.fillNewSchool(name,type,categorie))
+        .then(() => page.clickCreateButtonNewSchoolModal())
+        .then(() => sleep(500))
+        .then(() => searchForLeadSource(name))
+        .then(() => sleep(5000))
+        .then(() => {
+          var verificationText = page.getInnerHTML('/html/body/ui-view/app/div/div/div/div/ui-view/crm-lead-sources/div/div[2]/crm-lead-sources-results/div/div/div/table/tbody/tr/td[2]/a','xpath')
+          verificationText.txt.should.eventually.include(name, "The newly created school did not appear as the search result").notify(done);
+        })
+      });
+    }
+
+    var schoolCategories = ["Elementary School", "Middle School", "High School", "University", "Adult School"]
+    var schoolTypes = ["Public", "Private", "Catholic", "Charter", "Boarding", "Lutheran", "Christian", "Home"]
+
+    for (var i = schoolCategories.length - 1; i >= 0; i--) {
+      for (var x = schoolTypes.length - 1; x >= 0; x--) {
+        createSchoolTest(schoolCategories[i],schoolTypes[x])
+      }
+    }
+
+    it('Create a lead source and search for it', function(done){
+      this.retries(trys)
+      var name = page.randomString(10,"alpha");
+
+      page.clickCreateOption(3)
+      .then(() => page.fillNewLeadSource(name))
+      .then(() => page.clickCreateButtonNewLeadSourceModal())
+      .then(() => sleep(500))
+      .then(() => searchForLeadSource(name))
+      .then(() => sleep(5000))
+      .then(() => {
+        var verificationText = page.getInnerHTML('/html/body/ui-view/app/div/div/div/div/ui-view/crm-lead-sources/div/div[2]/crm-lead-sources-results/div/div/div/table/tbody/tr/td[2]/a','xpath')
+        verificationText.txt.should.eventually.include(name, "The newly created Lead source did not appear as the search result").notify(done);
+      })
+    });
   })
 }
 
